c498599629fe25d76c2cf6bb3056fb94b7221f9a
Roadagain <ringoh72@gmail.com>
2019-11-20 22:51:11 +0900

    簡単なページを作る

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <p>{{ count }}</p>
+    <button @click="onClick">count up</button>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    onClick() {
+      this.count++;
+    }
+  }
+};
+</script>
