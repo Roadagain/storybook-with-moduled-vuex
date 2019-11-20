e8f4fa5ca71f0df6375df6a072ed24d2fad7be53
Roadagain <ringoh72@gmail.com>
2019-11-20 23:07:47 +0900

    ボタンをcomponentsに切り出す

---
+++ components/CountUpButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="onClick">{{ count }}</button>
+</template>
+
+<script>
+export default {
+  computed: {
+    count() {
+      return this.$store.state.counter.count;
+    }
+  },
+  methods: {
+    onClick() {
+      this.$store.commit("counter/countUp");
+    }
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,15 @@
 <template>
   <main>
-    <p>{{ count }}</p>
-    <button @click="onClick">count up</button>
+    <count-up-button />
   </main>
 </template>
 
 <script>
+import CountUpButton from "~/components/CountUpButton";
+
 export default {
-  computed: {
-    count() {
-      return this.$store.state.counter.count;
-    }
-  },
-  methods: {
-    onClick() {
-      this.$store.commit("counter/countUp");
-    }
+  components: {
+    CountUpButton
   }
 };
 </script>
